ecf3ae7fba7b1d2affed975f83e63e65fe549edf
m7madmagdy <mohamedmagdysaber@gmail.com>
2023-01-29 15:11:36 +0200

    refactor

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
-    
-    def index 
-        @tasks = Task.all
-        @task = Task.new
-    end
+  before_action :set_task, only: %i[edit update destroy toggle]
 
-    def create
+  def index
+    @tasks = Task.all
+    @task = Task.new
+  end
+
+  def create
     @task = Task.new(task_params)
 
     respond_to do |format|
@@ -17,12 +18,9 @@ class TasksController < ApplicationController
     end
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     respond_to do |format|
       if @task.update(task_params)
         format.html { redirect_to tasks_url, notice: "Task was successfully updated" }
@@ -33,19 +31,22 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_url, notice: "Post was successfully deleted."
   end
 
   def toggle
-    @task = Task.find(params[:id])
     @task.update(completed: params[:completed])
     render json: { message: "Success" }
   end
 
   private
+
   def task_params
     params.require(:task).permit(:description)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
